604ef045e244b2f296e9889d558cb9a7122c7480
Shawn Reisner <shawn.reisner@gmail.com>
2025-07-23 13:09:51 -0500

    animate the bg

--- apps/glory_to_labor/src/components/Background.svelte
+++ apps/glory_to_labor/src/components/Background.svelte
@@ -6,6 +6,9 @@
 	import { getContext } from '../game/context';
 	import { COLORS } from '../game/constants';
 	import PropagandaBurst from './PropagandaBurst.svelte';
+	import { Tween } from 'svelte/motion';
+	import { mount, onMount } from 'svelte';
+	import { backInOut } from 'svelte/easing';
 
 	const context = getContext();
 	const backgroundProps = $derived(
@@ -14,14 +17,22 @@
 	const showBaseBackground = $derived(context.stateGame.gameType === 'basegame');
 	const showFeatureBackground = $derived(context.stateGame.gameType === 'freegame');
 
-	const { width, height } = context.stateLayoutDerived.canvasSizes();
+	const { width, height } = $derived(context.stateLayoutDerived.canvasSizes());
 
-	const size = Math.max(width, height);
+	const size = $derived(Math.max(width, height));
+
+	const rotation = new Tween((-15 * Math.PI) / 180);
+	const scale = new Tween(0.1);
+
+	onMount(() => {
+		rotation.set(0, { duration: 2000, easing: backInOut });
+		scale.set(1, { duration: 2000, easing: backInOut });
+	});
 </script>
 
 <Rectangle {...context.stateLayoutDerived.canvasSizes()} backgroundColor={COLORS.cream} zIndex={-3} />
 
-<Container x={width / 2} y={height / 2}>
+<Container x={width / 2} y={height / 2} rotation={rotation.current} scale={scale.current}>
 	<PropagandaBurst wedges={40} innerR={0} outerR={size} color={0x8f1b1b} />
 </Container>
 
